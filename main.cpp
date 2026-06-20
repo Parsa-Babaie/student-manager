e3f0be67456940e2c6b0e2cc12c399b3794009b7
Parsa Babaie <parsaaghababaie97@gmail.com>
2026-06-21 00:28:14 +0330

    Handle menu choices

--- main.cpp
+++ main.cpp
@@ -3,10 +3,27 @@
 using namespace std;
 
 int main(){
-
+    int choice;
+    
     cout<< "=== student manager ==="<<endl;
     cout<<"1. Add Student"<<endl;
     cout<<"2. Show students"<<endl;
     cout<<"3. Exit"<<endl;
+
+    cout<<"Enter your choice: ";
+    cin>>choice;
+
+    if(choice==1){
+        cout<<"Add Student selected."<<endl;
+    }
+    else if(choice==2){
+        cout<<"Show Students selected."<<endl;
+    }
+    else if(choice==3){
+        cout<<"Goodbye!"<<endl;
+    }
+    else{
+        cout<<"Invalid choice!"<<endl;
+    }
     return 0;
 }
