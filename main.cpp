26d1809d14ed9b8869e48854347d45206a9799f4
Parsa Babaie <parsaaghababaie97@gmail.com>
2026-06-20 14:42:12 +0330

    Add main menu

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(){
 
     cout<< "=== student manager ==="<<endl;
-
-
+    cout<<"1. Add Student"<<endl;
+    cout<<"2. Show students"<<endl;
+    cout<<"3. Exit"<<endl;
     return 0;
 }
