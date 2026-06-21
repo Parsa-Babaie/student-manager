b4bf0502d1fe7e1e8e0bb66fe962aca4c721dd0f
Parsa Babaie <parsaaghababaie97@gmail.com>
2026-06-21 22:49:32 +0330

    Create deleteStudent function

--- main.cpp
+++ main.cpp
@@ -43,12 +43,33 @@ void addStudent(vector<string>& students){
     cout<<"student "<<studentName<<" added successfully!"<<endl;
 }
 
+void deleteStudent(vector<string>& students){
+    int studentNumber;
+    if(students.empty()){
+        cout<<"NO student to delete!"<<endl;
+    }
+    else{
+        cout<<"\nStudents list: "<<endl;
+        for(int i=0 ; i < students.size() ; i++){
+            cout<<i+1<<". "<<students[i]<<endl;
+        }
+        cout<<"Enter student number to delete:";
+        cin>>studentNumber;
+        if(studentNumber >= 1 && studentNumber <= students.size()){
+            students.erase(students.begin() + studentNumber - 1);
+            saveStudents(students);
+            cout<<"student deleted successfully"<<endl;
+        } 
+        else{
+            cout<<"Invalid student number!"<<endl;
+        }
+    }
+}
 
 int main(){
     int choice;
     vector<string> students;
     loadStudents(students);
-    int studentNumber;
     while(true){
     showMenu();    
     cout<<"Enter your choice: ";
@@ -67,27 +88,9 @@ int main(){
                 cout<<i+1<<". "<< students[i] <<endl;
             }
         }
-    }
+    }    
     else if(choice == 3){
-        if(students.empty()){
-            cout<<"No student to delete."<<endl;
-        }
-        else{
-            cout<<"\nstudents list: "<<endl;
-            for(int i=0;i < students.size();i++){
-                cout<<i+1<<". "<<students[i]<<endl;
-            }
-            cout<<"Enter student number to delete: ";
-            cin>>studentNumber;
-            if(studentNumber >= 1 && studentNumber <= students.size()){
-                students.erase(students.begin()+studentNumber-1);
-                saveStudents(students);
-                cout<<"student earse successfully"<<endl;
-            }
-            else{
-                cout<<"Invalid student number!"<<endl;
-            }
-        }
+        deleteStudent(students);
     }
     else if(choice==4){
         cout<<"Goodbye!"<<endl;
