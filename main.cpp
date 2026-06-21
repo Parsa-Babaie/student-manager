0b306a76596fb2cb21a8d53710342fde1c7b4b58
Parsa Babaie <parsaaghababaie97@gmail.com>
2026-06-21 20:59:27 +0330

    Add delete student menu option

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@ int main(){
     cout<< "=== student manager ==="<<endl;
     cout<<"1. Add Student"<<endl;
     cout<<"2. Show students"<<endl;
-    cout<<"3. Exit"<<endl;
+    cout<<"3. Delete student"<<endl;
+    cout<<"4. Exit"<<endl;
 
     cout<<"Enter your choice: ";
     cin>>choice;
@@ -35,7 +36,7 @@ int main(){
             }
         }
     }
-    else if(choice==3){
+    else if(choice==4){
         cout<<"Goodbye!"<<endl;
         break;
     }
